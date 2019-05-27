9ee07991a802fecdb8d33a4488755aa0d2fc0adb
Markus Kullberg <kullberg.markus@gmail.com>
2019-05-27 21:34:22 +0300

    Cleanup

--- check-pvp-api/dist/check-pvp-api/src/app/routes/character.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("controllers");
-const router = express_1.Router();
-const controller = new controllers_1.CharacterController();
-router.get(`/:id`, controller.getCharacterData);
-exports.default = router;
-//# sourceMappingURL=character.router.js.map
--- check-pvp-api/dist/check-pvp-api/src/app/routes/recent-check.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("controllers");
-const router = express_1.Router();
-const controller = new controllers_1.RecentCheckController();
-router.get('/', controller.openStream);
-exports.default = router;
-//# sourceMappingURL=recent-check.router.js.map
